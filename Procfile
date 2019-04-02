c869b84faf536f74704241e608c3eccfc8ffc921
Sergey <sergeyplayinr2@gmail.com>
2019-04-02 21:57:56 +0300

    changed structure for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn authVK.wsgi --log-file -
--- authVK/settings.py
+++ authVK/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nbppm7_w@2ti^8&5i(47_0xl)@fbj!@ip&1i)#$y4o&n=i9fjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
