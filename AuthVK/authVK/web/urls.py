7a1144d22e00acf5605f162031694a5d17f7b71d
tolmachevsv <46191990+tolmachevsv@users.noreply.github.com>
2019-04-02 18:28:59 +0300

    Add files via upload

---
+++ AuthVK/authVK/web/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('info', views.info, name='info'),
+    path('auth', views.auth_with_code, name='auth'),
+    path('redirect', views.vk_redirect, name='vk_redirect'),
+    path('profile', views.profile, name='profile')
+]
---
+++ AuthVK/authVK/web/views.py
@@ -0,0 +1,135 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
+from django.template import loader
+
+import requests
+import datetime
+import vk
+
+access_token_cookie = 'vk_access_token'
+user_id_cookie = 'vk_user_id'
+
+client_id = 6920486
+secure_key = 'eIKDzRe3DyZrZIZEfdpz'
+redirect_uri = 'http://localhost:8000/auth'
+
+friends_count = 5
+fields = 'nickname, domain, sex, bdate, city, country, timezone'
+# Create your views here.
+
+#
+# def index(request):
+#     code = request.GET.get('code', None)
+#     if not code:
+#         template = loader.get_template('web/index.html')
+#         return HttpResponse(template.render({}, request))
+#
+#     token, user_id = get_access_token(code)
+#     if not token:
+#         return HttpResponseNotFound("Error while receiving token")
+#     session = vk.Session(access_token=token)
+#     api = vk.API(session, v=5)
+#
+#     user = api.users.get(user_ids=user_id)
+#     friends = api.friends.get(user_id=user_id, order='random', count=friends_count, fields=fields)
+#
+#     print(user)
+#     print(friends)
+#
+#     template = loader.get_template('web/friends.html')
+#
+#     return HttpResponse(template.render({
+#         'friends': friends['items'],
+#         'user': user[0]
+#     }, request))
+
+
+def index(request):
+    if access_token_cookie not in request.COOKIES:
+        template = loader.get_template('web/index.html')
+        return HttpResponse(template.render({}, request))
+
+    return HttpResponseRedirect("/info")
+
+
+def auth_with_code(request):
+    code = request.GET.get('code', None)
+    if not code:
+        return HttpResponseNotFound("Code not found")
+
+    token, user_id = get_access_token(code)
+    if not token:
+        return HttpResponseNotFound("Error while receiving token")
+
+    response = HttpResponseRedirect("/info")
+    set_cookie(response, access_token_cookie, token)
+    set_cookie(response, user_id_cookie, user_id)
+
+    return response
+
+
+def info(request):
+    if access_token_cookie not in request.COOKIES:
+        return HttpResponseNotFound("Access token not found in request, are you authenticated?")
+    if user_id_cookie not in request.COOKIES:
+        return HttpResponseNotFound("User id not found in request, are you authenticated?")
+
+    token = request.COOKIES[access_token_cookie]
+    user_id = request.COOKIES[user_id_cookie]
+
+    return render_info_page(token, user_id, request)
+
+
+def render_info_page(token, user_id, request):
+    session = vk.Session(access_token=token)
+    api = vk.API(session, v=5)
+
+    user = api.users.get(user_ids=user_id)
+    friends = api.friends.get(user_id=user_id, order='random', count=friends_count, fields=fields)
+
+    print(user)
+    print(friends)
+
+    template = loader.get_template('web/friends.html')
+
+    return HttpResponse(template.render({
+        'friends': friends['items'],
+        'user': user[0]
+    }, request))
+
+
+def vk_redirect(request):
+    resp_type = 'code'
+    scope = 'friends'
+    url = 'https://oauth.vk.com/authorize?client_id={}&redirect_uri={}&response_type={}&scope={}&v=5.92'.format(
+        client_id, redirect_uri, resp_type, scope
+    )
+    return HttpResponseRedirect(url)
+
+def profile(request):
+    template = loader.get_template('web/profile.html')
+
+    return HttpResponse(template.render({
+
+    }, request))
+
+def get_access_token(code):
+    url = "https://oauth.vk.com/access_token?client_id={}&client_secret={}&redirect_uri={}&code={}".format(
+        client_id, secure_key, redirect_uri, code
+    )
+    resp = requests.get(url)
+
+    data = resp.json()
+    if "error" in data:
+        return None, None
+
+    return data['access_token'], data['user_id']
+
+
+def set_cookie(response, key, value, days_expire = 1):
+    if days_expire is None:
+        max_age = 365 * 24 * 60 * 60  #one year
+    else:
+        max_age = days_expire * 24 * 60 * 60
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key, value, max_age=max_age, expires=expires)
